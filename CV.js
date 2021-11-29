9e8c6529257909b16a47c03092529214aad78514
shahar vazana <vazsha@post.bgu.ac.il>
2021-11-29 20:10:30 +0200

    targil 6

---
+++ CV.js
@@ -0,0 +1,7 @@
+   function myFunction() {
+     const inpObj = document.getElementById("id1");
+     if (!inpObj.checkValidity()) {
+        document.getElementById("demo").innerHTML = inpObj.validationMessage;} 
+     else {
+        document.getElementById("demo").innerHTML = "Input OK";}
+     }
